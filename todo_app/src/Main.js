7c1d9337bd561bdac803d6a1746269d16313d9ca
Jose <jose@cog.cr>
2019-09-03 11:27:36 -0600

    add new stuff

--- todo_app/src/Main.js
+++ todo_app/src/Main.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
+import "normalize.css";
 
-export default class Main extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Welcome to Movie App</h1>
-            </div>
-        )
-    }
-}
+const Main = () =>
+{
+
+    const APP_ID = 'be8178b7';
+    const APP_KEY = 'a6aa8ab438a034e2a95e8bc591c5cb3f';
+
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(()=> {
+        getRecipe();
+    }, []); // With the empty array, this ain't going to make a request every refresh.
+
+    const getRecipe = async () => {
+        const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        const data = await response.json();
+        setRecipes(data);
+    };
+
+
+    return (
+        <div>
+            <h1>Recipe App</h1>
+        </div>
+    );
+};
+
+export default Main;
